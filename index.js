9971d4e1370770cabac95c962075bf8fc13183f3
manshika140899 <manshika140899@gmail.com>
2026-09-09 23:37:20 +0500

    Fix vercel image serving

--- index.js
+++ index.js
@@ -112,12 +112,14 @@
 
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static("public"));
+// app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 // ===============================
 // ROUTES
